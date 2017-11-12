384f2e808f4e3efbcc4539555ee9b771015abdc6
Umesh Maharshi <umeshmaharshi@gmail.com>
2017-11-11 22:35:09 -0600

    Append Array to line

--- webCrawler/WebCrawler/src/FileUtil.java
+++ webCrawler/WebCrawler/src/FileUtil.java
@@ -11,25 +11,61 @@ public class FileUtil {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String filename = "sample.txt";
+		int lineNum = 1;
+		int linkIndex = -1;
+		int linkEnd = -1;
 		FileUtil fileUtil = new FileUtil();
 		ArrayList<String> strings = fileUtil.readFile(filename);
-		for (String string : strings) {
-			System.out.println(string);
+		String htmlString = fileUtil.concatArrayList(strings).trim().replaceAll(" +", " ");
+		linkIndex = htmlString.indexOf("<a href=");
+		while(linkIndex > -1) {
+			linkEnd = htmlString.indexOf("\"", linkIndex + 9);
+			System.out.println(htmlString.substring(linkIndex, linkEnd + 1));
+			linkIndex = htmlString.indexOf("<a href=", linkEnd);
 		}
+		/*
+		for (String string : strings) {
+			string = string.trim().replaceAll(" +", " ");
+			linkIndex = string.indexOf("<a href=");
+			linkEnd = string.indexOf("\"", linkIndex + 9);
+			if(linkIndex > 0) System.out.println(string.substring(linkIndex, linkEnd + 1));
+			lineNum++;
+		}
+		*/
 	}
-	
+
 	public ArrayList<String> readFile(String fileLocation) {
-		
+
 		ArrayList<String> fileLines = new ArrayList<String>();
+		Boolean pTagCheck = false;
+		Boolean topicsCheck = false;
 		try (BufferedReader br = Files.newBufferedReader(Paths.get(fileLocation))) {
 			String line;
 			while ((line = br.readLine()) != null) {
-			    fileLines.add(line);
+				//System.out.println(line + " " + line.contains("<p>"));
+				if(line.contains("<p>")) {
+					pTagCheck = true; 
+					fileLines.add(line); 
+				}
+				if(pTagCheck) {
+					while((line = br.readLine()) != null) { 
+						fileLines.add(line);
+					}
+				}
 			}
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 		return fileLines;
+	}
+	
+	public String concatArrayList(ArrayList<String> htmlLines) {
+		StringBuilder strBuilder = new StringBuilder();
+		for (int i = 0; i < htmlLines.size(); i++) {
+		   strBuilder.append(htmlLines.get(i));
+		}
+		return strBuilder.toString();
 	}
 
 }
