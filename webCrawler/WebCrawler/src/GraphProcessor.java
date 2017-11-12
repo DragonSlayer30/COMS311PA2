6a92ac38b9b48e6e50a52676e33da9b730ee7665
Your Name <you@example.com>
2017-11-11 21:12:28 -0600

    intial commit

---
+++ webCrawler/WebCrawler/src/GraphProcessor.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+
+public class GraphProcessor
+{
+
+	// NOTE: graphData should be an absolute file path
+	public GraphProcessor(String graphData)
+	{
+	}
+
+	public int outDegree(String v)
+	{
+		return 0;
+		
+	}
+
+	public ArrayList<String> bfsPath(String u, String v)
+	{
+		return null;
+	}
+
+	public int diameter()
+	{
+		return 0;
+	}
+
+	public int centrality(String v)
+	{
+		return 0;
+	}
+
+}
