71b204ea5d4c48efbef0300b2f4710779948787a
Umesh <you@example.com>
2017-11-15 01:22:44 -0600

    Different BFS

--- webCrawler/WebCrawler/src/GraphProcessor.java
+++ webCrawler/WebCrawler/src/GraphProcessor.java
@@ -14,13 +14,14 @@ public class GraphProcessor
 	int vertices = 0;
 	int edges = 0;
 	private Queue<String> queue;
-	
+
 	// NOTE: graphData should be an absolute file path
 	public GraphProcessor(String graphData) {
 		ArrayList<String> file = fileHelper.readFile(graphData);
 		int totalLength = file.size();
 		vertices = Integer.parseInt(file.get(0));
 		for(int i = 1; i < totalLength; i++) {
+			edges++;
 			String[] line = file.get(i).split(" ");
 			if(!graph.containsKey(line[0])) {
 				HashSet<String> ed = new HashSet<String>();
@@ -38,91 +39,100 @@ public class GraphProcessor
 	}
 
 	public ArrayList<String> bfsPath(String u, String v) {
-		int number_of_nodes = graph.get(u).size() - 1;
-		 
-        HashSet<String> visited = new HashSet<String>();
-        int i;
-        String element;
-        visited.add(u);
-        queue.add(u);
-        ArrayList<String> oneLevelParent = new ArrayList<String>();
-        oneLevelParent.add(u);
-        while (!queue.isEmpty()) {
-        	ArrayList<String> childPoss = new ArrayList<String>();
-            element = queue.remove();
-            i = 0;
-            //System.out.print(element + "\t");
-            while (i <= number_of_nodes)
-            {
-                if(graph.get(element) != null) {
-                	for (String p : oneLevelParent) {
-                		int cutIndex = p.lastIndexOf("#");
-                		String key = null;
-                		if(cutIndex < 0) {
-                			key = p;
-                		}
-                		else key = p.substring(cutIndex + 1);
-                		System.out.println("Parent : " + p + " cutindex : " + cutIndex + " " + key);
-                		if(graph.get(key) != null) {
-                			for (String string : graph.get(key)) {
-                    			if(string.equals(v)) {
-                    				String[] pathList = p.concat("#" + v).split("#");
-                    				return new ArrayList<String>(Arrays.asList(pathList));
-                    			}
-            					if(!visited.contains(string)) {
-            						visited.add(string);
-            						childPoss.add(p.concat("#" + string));
-            						queue.add(string);
-            					}
-            				}
-                		}
+		HashSet<String> visited = new HashSet<String>();
+		int i;
+		visited.add(u);
+		queue.add(u);
+		ArrayList<String> oneLevelParent = new ArrayList<String>();
+		oneLevelParent.add(u);
+		ArrayList<String> childPoss = new ArrayList<String>();
+		i = 0;
+		int totalSize = graph.size();
+		//System.out.print(element + "\t");
+		while(i < edges) {
+			System.out.println("index i : " + i + " " + totalSize);
+			for (String p : oneLevelParent) {
+				int cutIndex = p.lastIndexOf("#");
+				String key = null;
+				if(cutIndex < 0) {
+					key = p;
+				}
+				else key = p.substring(cutIndex + 1);
+				System.out.println("Parent : " + p + " cutindex : " + cutIndex + " " + key);
+				if(graph.get(key) != null) {
+					for (String string : graph.get(key)) {
+						i++;
+						System.out.println("Path from : " + key + " " + string);
+						if(string.equals(v)) {
+							String[] pathList = p.concat("#" + v).split("#");
+							return new ArrayList<String>(Arrays.asList(pathList));
+						}
+						if(!visited.contains(string)) {
+							visited.add(string);
+							System.out.println("Adding child : " + p.concat("#" + string));
+							childPoss.add(p.concat("#" + string));
+						}
 					}
-                }
-                i++;
-            }
-            oneLevelParent.clear();
-            oneLevelParent.addAll(childPoss);
-        }
+				}
+				i++;
+			}
+			oneLevelParent.clear();
+			oneLevelParent.addAll(childPoss);
+			childPoss.clear();
+		}
 		return new ArrayList<String>();
 	}
 
 	public int diameter() {
-		return 0;
+		int diameter = 0;
+		for (String vertex : graph.keySet()) {
+			for (String vertex2 : graph.keySet()) {
+				if(!vertex2.equals(vertex)) {
+					int d = bfsPath(vertex, vertex2).size();
+					if(d == 0) { 
+						System.out.println("No path between : " + vertex + "  " + vertex2);
+						return graph.size()*2;
+					}
+					if(d > diameter) diameter = d;
+				}
+			}
+		}
+		return diameter;
 	}
 
 	public int centrality(String v) {
 		return 0;
 	}
-	
+
 	public void bfs(String p1)
-    {
-        int number_of_nodes = graph.get(p1).size() - 1;
- 
-        HashSet<String> visited = new HashSet<String>();
-        int i;
-        String element;
-        visited.add(p1);
-        queue.add(p1);
- 
-        while (!queue.isEmpty()) {
-            element = queue.remove();
-            i = 0;
-            System.out.print(element + "\t");
-            while (i <= number_of_nodes)
-            {
-                if(graph.get(element) != null) {
-                	for (String string : graph.get(element)) {
-    					if(!visited.contains(string)) {
-    						visited.add(string);
-    						queue.add(string);
-    					}
-    				}
-                	i++;
-                }
-                else break;
-            }
-        }
-    }
-	
+	{
+		int number_of_nodes = graph.get(p1).size() - 1;
+
+		HashSet<String> visited = new HashSet<String>();
+		int i;
+		String element;
+		visited.add(p1);
+		queue.add(p1);
+
+		while (!queue.isEmpty()) {
+			element = queue.remove();
+			i = 0;
+			System.out.print(element + "\t");
+			while (i <= number_of_nodes)
+			{
+				if(graph.get(element) != null) {
+					for (String string : graph.get(element)) {
+						if(!visited.contains(string)) {
+							visited.add(string);
+							queue.add(string);
+						}
+					}
+					i++;
+				}
+				else break;
+			}
+		}
+	}
+
 
 }
--- webCrawler/WebCrawler/src/TestGraphProcessor.java
+++ webCrawler/WebCrawler/src/TestGraphProcessor.java
@@ -16,8 +16,8 @@ public class TestGraphProcessor {
 		System.out.println("Total edges : " + totalEdges);
 		//String p1 = "/wiki/Lorenz_system";
 		//String p2 = "/wiki/Complexity_theory";
-		String p1 = "Minneapolis";
-		String p2 = "Omaha";
+		String p1 = "Omaha";
+		String p2 = "";
 		//String p1 = "U";
 		//String p2 = "H";
 		ArrayList<String> path = graphProcessor.bfsPath(p1, p2);
@@ -27,6 +27,7 @@ public class TestGraphProcessor {
 		for (String string : path) {
 			System.out.println(string);
 		}
+		//System.out.println("Diameter : " + graphProcessor.diameter());
 		//graphProcessor.bfs(p1);
 	}
 
